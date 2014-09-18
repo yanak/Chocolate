79a788cdce8dea973c7c83a9b15e7589293313ed
Yasunori Tanaka <yasunori.tanaka@enish.com>
2014-09-18 21:33:08 +0900

    add a create-message method

--- lib/chocolate/chatwork_keeper.rb
+++ lib/chocolate/chatwork_keeper.rb
@@ -3,15 +3,15 @@ require 'chocolate/chatwork'
 class ChatworkKeeper
 
   def initialize
-    @chatwork = ChatWork.new
+    @chatwork = Chatwork.new
   end
 
   def retrieve_message
-
+    return @chatwork.retrieve_messages(10)
   end
 
-  def create_message
-
+  def create_message(message)
+    @chatwork.create_message(message)
   end
 
 end
